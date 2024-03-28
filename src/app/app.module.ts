12b34e33aa09a5220a3d9111b57cbf2646bd8795
Pawel Felcyn <pawel.felcyn@onet.pl>
2024-03-28 17:58:02 +0100

    #10: add login page mock to the admin panel

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'dashboard', component: DashboardComponent},
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterOutlet,
+    RouterModule.forRoot(routes),
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  declarations: [AppComponent, LoginComponent, DashboardComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    provideAnimationsAsync()
+  ],
+})
+export class AppModule {}
