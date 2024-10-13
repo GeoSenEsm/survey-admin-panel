e7843a3ec7b733caddc68c6454e86005f9a77692
Pawel Felcyn <pawel.felcyn@onet.pl>
2024-10-13 11:41:05 +0200

    validate questions not to repeat

--- src/presentation/modules/app/components/start-survey-question/start-survey-question.component.ts
+++ src/presentation/modules/app/components/start-survey-question/start-survey-question.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { StartSurveyOption, StartSurveyQuestion } from '../../../../../core/models/start-survey-question';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormlessErrorStateMatcher } from '../../../../utils/formless.error.state.matcher';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-start-survey-question',
@@ -11,6 +14,12 @@ export class StartSurveyQuestionComponent {
   @Input() isReadOnly: boolean = false;
   readonly maxInputLen = 250;
   @Output() removeQuestionCallback = new EventEmitter<StartSurveyQuestion>();
+  @Input() allOldQuestions: StartSurveyQuestion[] | undefined = undefined;
+  @Input() allNewQuestions: StartSurveyQuestion[] | undefined = undefined;
+  questionError: string | null = null
+  questionContentErrorStateMatcher: ErrorStateMatcher = new FormlessErrorStateMatcher(() => this.questionError);
+
+  constructor(private readonly translate: TranslateService){}
 
   addOption(): void {
     if (this.question) {
@@ -27,6 +36,24 @@ export class StartSurveyQuestionComponent {
   removeQusetion(): void{
     if (this.question) {
       this.removeQuestionCallback.emit(this.question);
+    }
+  }
+
+  validateQuestion(): void{
+    this.questionError = null;
+
+    if (!this.question){
+      return;
+    }
+
+    if (this.question.content.length == 0){
+      this.questionError = this.translate.instant('startSurvey.questionCannotBeEmpty');
+      return;
+    }
+
+    if ((this.allOldQuestions && this.allOldQuestions.find(e => e != this.question && e.content.trim() == this.question!.content.trim()))
+    || (this.allNewQuestions && this.allNewQuestions.find(e => e != this.question && e.content.trim() == this.question!.content.trim()))){
+      this.questionError = this.translate.instant('startSurvey.questionAlreadyExists');
     }
   }
 }
