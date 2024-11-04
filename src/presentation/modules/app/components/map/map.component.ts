f25b5c8b201da71329ee4a14b9108cfeec74b293
Pawel Felcyn <pawel.felcyn@onet.pl>
2024-11-04 18:37:48 +0100

    display map

--- src/presentation/modules/app/components/map/map.component.ts
+++ src/presentation/modules/app/components/map/map.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  private map : L.Map | undefined;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [52.2297, 21.0122],
+      zoom: 13
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+
 
 }
