1fc70c6f584bd3310196e7292b92369aea31ad8e
Pawel Felcyn <pawel.felcyn@onet.pl>
2024-11-13 19:25:54 +0100

    validate code

--- src/core/models/image_option.ts
+++ src/core/models/image_option.ts
@@ -1,4 +1,5 @@
 export interface ImageOption{
     code?: string,
-    file?: File
+    file?: File,
+    src?: string
 }
--- src/presentation/modules/app/components/image-upload/image-upload.component.ts
+++ src/presentation/modules/app/components/image-upload/image-upload.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UntypedFormBuilder } from '@angular/forms';
+import { ImageOption } from '../../../../../core/models/image_option';
+import { FormlessErrorStateMatcher } from '../../../../utils/formless.error.state.matcher';
 
 @Component({
   selector: 'app-image-upload',
@@ -8,12 +10,14 @@ import { UntypedFormBuilder } from '@angular/forms';
 })
 export class ImageUploadComponent {
   readonly maxCodeLength = 20;
+  @Input()
+  imageOption: ImageOption | undefined;
+  @Input()
+  allImageOptions: ImageOption[] | undefined;
+  codeError: string | null = null;
+  codeErrorStateMatcher = new FormlessErrorStateMatcher(() => this.codeError);
 
-  editForm = this.fb.group({
-    photo: []
-  });
-
-  constructor(private fb: UntypedFormBuilder) { }
+  constructor() { }
 
 
   setFileData(event: Event): void {
@@ -22,9 +26,47 @@ export class ImageUploadComponent {
       const file: File = eventTarget.files[0];
       const reader = new FileReader();
       reader.addEventListener('load', () => {
-        this.editForm.get('photo')?.setValue(reader.result as string);
+        if (this.imageOption){
+          this.imageOption.file = file;
+          this.imageOption.src = reader.result as string;
+        }
       });
       reader.readAsDataURL(file);
     }
+  }
+
+  clearFile(): void{
+    if (this.imageOption){
+      this.imageOption.file = undefined;
+      this.imageOption.src = undefined;
+    }
+  }
+
+  validateCode(): boolean{
+    this.codeError = null;
+
+    if (!this.imageOption){
+      return true;
+    }
+
+    if (!this.imageOption.code){
+      this.codeError = "createSurvey.createImageOptions.codeRequiredError";
+      return false;
+    }
+
+    if (!this.allImageOptions){
+      return true;
+    }
+
+    const isDuplicate = this.allImageOptions.some(
+      option => option.code === this.imageOption!.code && option !== this.imageOption
+    );
+
+    if (isDuplicate){
+      this.codeError = "createSurvey.createImageOptions.codeDuplicateError";
+      return false;
+    }
+
+    return false;
   }
 }
