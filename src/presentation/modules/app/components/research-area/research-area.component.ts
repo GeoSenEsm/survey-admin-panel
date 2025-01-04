7e3b0b2a6bb491f258e3adf71e3952582f3911af
pawelFelcyn <89747833+pawelFelcyn@users.noreply.github.com>
2025-01-04 14:56:06 +0100

    #182: use 6 decimal points in research area config (#66)

--- src/presentation/modules/app/components/research-area/research-area.component.ts
+++ src/presentation/modules/app/components/research-area/research-area.component.ts
@@ -1,6 +1,14 @@
-import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import L, { LatLng } from 'leaflet';
-import { Papa } from 'ngx-papaparse';
+import { Papa, ParseResult } from 'ngx-papaparse';
 import { LatLong } from '../../../../../domain/models/lat_long';
 import { RESEARCH_AREA_SERVICE_TOKEN } from '../../../../../core/services/injection-tokens';
 import { ResearchAreaService } from '../../../../../domain/external_services/research_area.service';
@@ -11,7 +19,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-research-area',
   templateUrl: './research-area.component.html',
-  styleUrl: './research-area.component.scss'
+  styleUrl: './research-area.component.scss',
 })
 export class ResearchAreaComponent implements OnInit, OnDestroy, AfterViewInit {
   private map: L.Map | undefined;
@@ -22,19 +30,20 @@ export class ResearchAreaComponent implements OnInit, OnDestroy, AfterViewInit {
   errorOnLoadingCurrentResearchArea: boolean = false;
   private rememberedNodes: LatLong[] | undefined;
 
-
-  constructor(private papa: Papa,
-    @Inject(RESEARCH_AREA_SERVICE_TOKEN) private readonly researchAreaService: ResearchAreaService,
+  constructor(
+    private papa: Papa<LatLng>,
+    @Inject(RESEARCH_AREA_SERVICE_TOKEN)
+    private readonly researchAreaService: ResearchAreaService,
     private readonly translate: TranslateService,
     private readonly snackbar: MatSnackBar
-  ) {} 
-  
+  ) {}
+
   ngAfterViewInit(): void {
     this.initMap();
   }
-  
+
   ngOnDestroy(): void {
-    if (this.map){
+    if (this.map) {
       this.map.remove();
     }
   }
@@ -61,25 +70,25 @@ export class ResearchAreaComponent implements OnInit, OnDestroy, AfterViewInit {
     this.errorOnLoadingCurrentResearchArea = false;
 
     this.researchAreaService
-    .getResearchArea()
-    .pipe(
-      catchError((error) => {
-        if (error.status ==  404){
-          return of([]);
-        }
+      .getResearchArea()
+      .pipe(
+        catchError((error) => {
+          if (error.status == 404) {
+            return of([]);
+          }
 
-        return throwError(() => error);
-      })
-    )
-    .subscribe({
-      next: (data) => {
-        this.drawPolygon(data);
-      },
-      error: (error) => {
-        this.errorOnLoadingCurrentResearchArea = true;
-        console.log(error);
-      }
-    })
+          return throwError(() => error);
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          this.drawPolygon(data);
+        },
+        error: (error) => {
+          this.errorOnLoadingCurrentResearchArea = true;
+          console.log(error);
+        },
+      });
   }
 
   loadFromFile(): void {
@@ -94,36 +103,64 @@ export class ResearchAreaComponent implements OnInit, OnDestroy, AfterViewInit {
         header: true,
         skipEmptyLines: true,
         complete: (result) => {
-          const data: LatLong[] = result.data as LatLong[];
+          if (
+            result.data.some(
+              (e: any) => e.longitude == undefined || e.latitude == undefined
+            )
+          ) {
+            this.showInvalidFormat();
+            return;
+          }
+
+          const data: LatLong[] = (result.data as LatLong[]).map((row) => ({
+            latitude: this.withPrecision(row.latitude, 6),
+            longitude: this.withPrecision(row.longitude, 6),
+          }));
           this.drawPolygon(data);
           this.changesMade = true;
         },
         error: (error) => {
-          const message = this.translate.instant('configuration.researchArea.somethingWentWrong');
+          const message = this.translate.instant(
+            'configuration.researchArea.somethingWentWrong'
+          );
           this.showOkMessage(message);
           console.error('Error parsing CSV file:', error);
-        }
+        },
       });
     }
   }
 
+  showInvalidFormat(): void {
+    const message = this.translate.instant(
+      'configuration.researchArea.invalidFormat'
+    );
+    this.showOkMessage(message);
+  }
+
+  withPrecision(value: number | string, precision: number): number {
+    if (typeof value === 'string') value = parseFloat(value as string);
+    return Number(value.toFixed(precision));
+  }
+
   private drawPolygon(vertices: LatLong[]): void {
     try {
-      if (this.nodes && !this.rememberedNodes){
+      if (this.nodes && !this.rememberedNodes) {
         this.rememberedNodes = this.nodes;
       }
       this.removePolygon();
       this.nodes = vertices;
-      if (vertices.length === 0){
+      if (vertices.length === 0) {
         return;
       }
-      const latLngs = vertices.map(vertex => new LatLng(vertex.latitude, vertex.longitude));
+      const latLngs = vertices.map(
+        (vertex) => new LatLng(vertex.latitude, vertex.longitude)
+      );
       this.researchAreaPolygon = L.polygon(latLngs, {
         color: 'darkblue',
         fillColor: 'blue',
-        fillOpacity: 0.5
+        fillOpacity: 0.5,
       });
-  
+
       this.researchAreaPolygon.addTo(this.map!);
       this.map?.fitBounds(this.researchAreaPolygon.getBounds());
     } finally {
@@ -139,58 +176,66 @@ export class ResearchAreaComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   resetChanges(): void {
-    if (this.rememberedNodes){
+    if (this.rememberedNodes) {
       this.drawPolygon(this.rememberedNodes);
       this.changesMade = false;
     }
   }
 
   save(): void {
-    if (this.nodes){
+    if (this.nodes) {
       this.errorOnLoadingCurrentResearchArea = false;
-      this.researchAreaService
-        .upsert(this.nodes)
-        .subscribe({
-          next: _ =>{
-            this.changesMade = false;
-            this.rememberedNodes = this.nodes;
-          },
-          error: (e) => {
-            const message = this.translate.instant('configuration.researchArea.errorOnSavingChanges');
-            this.showOkMessage(message);
-            console.log(e);
-          }
-        });
+      this.researchAreaService.upsert(this.nodes).subscribe({
+        next: (_) => {
+          this.changesMade = false;
+          this.rememberedNodes = this.nodes;
+        },
+        error: (e) => {
+          const message = this.translate.instant(
+            'configuration.researchArea.errorOnSavingChanges'
+          );
+          this.showOkMessage(message);
+          console.log(e);
+        },
+      });
     }
   }
 
   deleteResearchArea(): void {
     this.errorOnLoadingCurrentResearchArea = false;
-    this.researchAreaService.remove()
-      .subscribe({
-      next: _ =>{
+    this.researchAreaService.remove().subscribe({
+      next: (_) => {
         this.changesMade = false;
         this.rememberedNodes = undefined;
         this.drawPolygon([]);
       },
       error: (error) => {
-        const message = this.translate.instant('configuration.researchArea.errorOnDeleting');
+        const message = this.translate.instant(
+          'configuration.researchArea.errorOnDeleting'
+        );
         this.showOkMessage(message);
         console.log(error);
-      }
+      },
     });
   }
 
-  private showOkMessage(message: string){
+  private showOkMessage(message: string) {
     const ok = this.translate.instant('configuration.ok');
     this.snackbar.open(message, ok, { duration: 3000 });
   }
 
-  canDelete(): boolean{
-    return this.areaDefined() &&  (this.rememberedNodes ?? true) && this.rememberedNodes?.length !== 0;
+  canDelete(): boolean {
+    return (
+      this.areaDefined() &&
+      (this.rememberedNodes ?? true) &&
+      this.rememberedNodes?.length !== 0
+    );
   }
 
-  areaDefined(): boolean{
-    return this.errorOnLoadingCurrentResearchArea || (this.nodes !== undefined && this.nodes.length !== 0);
+  areaDefined(): boolean {
+    return (
+      this.errorOnLoadingCurrentResearchArea ||
+      (this.nodes !== undefined && this.nodes.length !== 0)
+    );
   }
 }
