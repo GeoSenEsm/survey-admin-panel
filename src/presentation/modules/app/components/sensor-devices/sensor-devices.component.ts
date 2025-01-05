39023276c865f42884cb6753072413536a95d789
Pawel Felcyn <pawel.felcyn@onet.pl>
2025-01-05 22:34:57 +0100

    deleting

--- src/presentation/modules/app/components/sensor-devices/sensor-devices.component.ts
+++ src/presentation/modules/app/components/sensor-devices/sensor-devices.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { SensorDto } from '../../../../../domain/models/sensors-dtos';
 import { MatTableDataSource } from '@angular/material/table';
 import { SensorsService } from '../../../../../domain/external_services/sensors.service';
@@ -6,6 +12,8 @@ import { SENSORS_SERVICE_TOKEN } from '../../../../../core/services/injection-to
 import { finalize } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { SensorsImportProgressIndicatorComponent } from '../sensors-import-progress-indicator/sensors-import-progress-indicator.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-devices',
@@ -21,9 +29,11 @@ export class SensorDevicesComponent implements OnInit {
   constructor(
     @Inject(SENSORS_SERVICE_TOKEN)
     private readonly sensorsService: SensorsService,
-    private readonly matDialog: MatDialog
+    private readonly matDialog: MatDialog,
+    private readonly snackbar: MatSnackBar,
+    private readonly translate: TranslateService
   ) {}
-  
+
   ngOnInit(): void {
     this.loadData();
   }
@@ -45,17 +55,44 @@ export class SensorDevicesComponent implements OnInit {
     this.fileInput.nativeElement.click();
   }
 
-  public onImport(event: Event){
+  public onImport(event: Event) {
     this.matDialog.open(SensorsImportProgressIndicatorComponent, {
       data: {
         fileSelectionEvent: event,
         currentData: this.dataSource.data,
         reloadCallback: () => this.loadData(),
-      }
+      },
     });
   }
 
   public edit(sensor: SensorDto): void {}
 
-  public delete(sensor: SensorDto): void {}
+  public delete(sensor: SensorDto): void {
+    if (this.isBusy) {
+      return;
+    }
+
+    this.isBusy = true;
+    this.sensorsService
+      .deleteSensor(sensor.sensorId)
+      .pipe(finalize(() => (this.isBusy = false)))
+      .subscribe({
+        next: () => {
+          const idx = this.dataSource.data.findIndex(
+            (item) => item.id === sensor.id
+          );
+          if (idx !== -1) {
+            this.dataSource.data.splice(idx, 1);
+            this.dataSource.data = [...this.dataSource.data];
+          }
+        },
+        error: (e) => {
+          console.log(e);
+          this.snackbar.open(
+            this.translate.instant('sensorDevices.couldNotDelete'),
+            'OK'
+          );
+        },
+      });
+  }
 }
