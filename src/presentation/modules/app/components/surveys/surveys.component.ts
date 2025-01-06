6bdb17d9ad21ad8573c3521c3e470e7c34e29853
Pawel Felcyn <pawel.felcyn@onet.pl>
2025-01-06 17:25:53 +0100

    #190: show information that there are no surveys created

--- src/presentation/modules/app/components/surveys/surveys.component.ts
+++ src/presentation/modules/app/components/surveys/surveys.component.ts
@@ -4,37 +4,52 @@ import { Router } from '@angular/router';
 import { SurveyDto } from '../../../../../domain/models/survey.dto';
 import { SurveyService } from '../../../../../domain/external_services/survey.service';
 import { TranslateService } from '@ngx-translate/core';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-surveys',
   templateUrl: './surveys.component.html',
-  styleUrl: './surveys.component.css'
+  styleUrl: './surveys.component.css',
 })
-export class SurveysComponent implements OnInit{
-
+export class SurveysComponent implements OnInit {
   surveys: SurveyDto[] = [];
+  isBusy = false;
+  loadingErrorOccured = false;
 
-  constructor(private readonly router: Router,
-    @Inject('surveyService')private readonly surveyService: SurveyService){
-    }
-  
+  constructor(
+    private readonly router: Router,
+    @Inject('surveyService') private readonly surveyService: SurveyService
+  ) {}
+
   ngOnInit(): void {
     this.loadSurveys();
   }
 
-  loadSurveys(): void{
+  loadSurveys(): void {
+    if (this.isBusy){
+      return;
+    }
+    this.isBusy = true;
     this.surveys.length = 0;
+    this.loadingErrorOccured = false;
 
-    this.surveyService
-    .getAllShort()
-    .subscribe(result =>{
-      result.forEach(survey =>{
-        this.surveys.push(survey);
-      });
+    
+    this.surveyService.getAllShort()
+    .pipe(finalize(() => (this.isBusy = false)))
+    .subscribe({
+      next: (result) => {
+        result.forEach((survey) => {
+          this.surveys.push(survey);
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        this.loadingErrorOccured = true;
+      }
     });
   }
 
-  createNew(): void{
+  createNew(): void {
     this.router.navigate(['surveys/new']);
   }
 }
